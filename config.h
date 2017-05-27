ac16cf86f648c49ce64a1e50862ad48c461ed137
Tyler Earnest <tylere@rne.st>
2017-05-26 21:30:24 -0500

    bstack

--- config.h
+++ config.h
@@ -45,6 +45,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
+	{ "TTT",      bstack },
 	{ "[M]",      monocle },
 };
 
